36999e1a56ba48c36f3342d36d645e7a453eabac
Cristiano Cucco <cristiano.cucco@gmail.com>
2015-06-29 23:15:44 +0200

    added some meat. Launch "grunt build" to compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,63 @@
 module.exports = function(grunt) {
 
+    // Configurable paths for the application
+    var appConfig = {
+        app: require('./bower.json').appPath || 'app',
+        dist: 'dist'
+    };
+
     // 1. All configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-            dist: {
-                src: [
-                    'js/libs/*.js', // All JS in the libs folder
-                    'js/global.js'  // This specific file
-                ],
-                dest: 'js/build/production.js'
+        useminPrepare: {
+            html: 'app/*.html',
+            options:{
+                dest: 'dist'
+            }
+        },
+        usemin: {
+            html: 'dist/*.html',
+        },
+        copy: {
+            app: {
+                expand: true,
+                cwd: 'app/',
+                src: ['*.html'],
+                dest: 'dist/'
             }
         },
 
-        uglify: {
-            build: {
-                src: 'js/build/production.js',
-                dest: 'js/build/production.min.js'
-            }
+        // Empties folders to start fresh
+        clean: {
+            server: '.tmp'
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-usemin');
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-clean');
+    //grunt.loadNpmTasks('grunt-filerev');
+    // Load all files starting with `grunt-`
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    //grunt.registerTask('default', ['concat', 'uglify']);
+
+    // simple build task
+    grunt.registerTask('build', [
+        'copy:app',
+        'useminPrepare',
+        'concat:generated',
+        //'cssmin:generated',
+        'uglify:generated',
+        'clean',
+        //'filerev',
+        'usemin'
+    ]);
 
 };
---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cris on 29/06/2015.
+ */
+(function($){
+    window.App = window.App || {};
+    App.setTitle = function(title){
+        $('#title').html(title);
+    }
+})(jQuery);
